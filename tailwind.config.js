703286849a0a9ce5dd61fe5bebb539382d169cb1
brewea <brewea@gmail.com>
2021-08-04 13:32:36 -0400

    React - first project, todo list

---
+++ tailwind.config.js
@@ -0,0 +1,34 @@
+const colors = require("tailwindcss/colors");
+
+module.exports = {
+  purge: ["./src/**/*.{js,jsx,ts,tsx}", "./public/index.html"],
+  darkMode: false, // or 'media' or 'class'
+  theme: {
+    colors: {
+      gray: colors.coolGray,
+      blue: colors.sky,
+      red: colors.rose,
+      pink: colors.fuchsia,
+    },
+    fontFamily: {
+      sans: ["Graphik", "sans-serif"],
+      serif: ["Merriweather", "serif"],
+    },
+    extend: {
+      spacing: {
+        128: "32rem",
+        144: "36rem",
+      },
+      borderRadius: {
+        "4xl": "2rem",
+      },
+    },
+    variants: {
+      extend: {
+        borderColor: ["focus-visible"],
+        opacity: ["disabled"],
+      },
+    },
+    plugins: [],
+  },
+};
